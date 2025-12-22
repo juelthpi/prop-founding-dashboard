a8207acff5b58d5c918547932f3339cc2106256f
Softwarezon <softwarezon@Softwarezons-Mac-001.local>
2025-12-22 15:48:34 +0600

    design update V-1.0

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -9,3 +9,22 @@ document.getElementById("themeToggle")?.addEventListener("click", () => {
 // Load saved theme
 if (localStorage.getItem("theme") === "light")
   document.body.classList.add("light-mode");
+
+// Preloader js
+document.addEventListener("DOMContentLoaded", () => {
+  const progress = document.querySelector(".page-load-progress");
+  let width = 0;
+  const demoLoad = setInterval(() => {
+    if (width < 90) {
+      width += Math.random() * 10;
+      progress.style.width = width + "%";
+    }
+  }, 300);
+  window.addEventListener("load", () => {
+    clearInterval(demoLoad);
+    progress.style.width = "100%";
+    setTimeout(() => {
+      document.body.classList.add("page-load-loaded");
+    }, 500);
+  });
+});
